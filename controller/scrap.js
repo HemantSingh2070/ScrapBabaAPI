4b66b926216639ff8498eec55fa1790ab862494b
HemantSingh2070 <hemantsingh2070@gmail.com>
2024-11-29 01:45:13 +0530

    uploaded to be json format

--- controller/scrap.js
+++ controller/scrap.js
@@ -7,19 +7,19 @@ const {
 const AddScrap = async (req, res) => {
   const scrap = req.body;
   const result = await handleAddScrap(scrap);
-  res.status(201).send({ result });
+  res.status(201).json({ result });
 };
 
 const GetScraps = async (req, res) => {
   const { location } = req.body;
   const result = await handleGetScraps(location);
-  res.status(200).send({ result });
+  res.status(200).json({ result });
 };
 
 const RemoveScrap = async (req, res) => {
   const { id } = req.body;
   const result = await handleRemoveScrap(id);
-  res.status(200).send({ result });
+  res.status(200).json({ result });
 };
 
 module.exports = { AddScrap, GetScraps, RemoveScrap };
--- controller/user.js
+++ controller/user.js
@@ -5,9 +5,9 @@ const AddUser = async (req, res) => {
   const { name, phone, city } = req.body;
   try {
     const userId = await handleAddUser(name, phone, city);
-    res.status(201).send({ userId });
+    res.status(201).json({ userId });
   } catch (e) {
-    res.status(500).send({ error: e });
+    res.status(500).json({ error: e });
   }
 };
 
