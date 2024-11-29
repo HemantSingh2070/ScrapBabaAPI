cfa4130791d0af67577c333f60c5ceb3efb2d324
HemantSingh2070 <hemantsingh2070@gmail.com>
2024-11-29 19:34:55 +0530

    done

--- services/message.js
+++ services/message.js
@@ -1,14 +1,14 @@
 const twilio = require("twilio");
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID; // Ensure this is set in your environment variables
-const authToken = "7fadd660fe778ad2a0aa04176c501d39"; // Secure this value using environment variables
+const authToken = process.env.TWILIO_AUTH_TOKEN; // Secure this value using environment variables
 const client = twilio(accountSid, authToken);
 
 const sendMessage = async (messageBody, toPhoneNumber) => {
   try {
     const message = await client.messages.create({
       body: messageBody,
-      from: "+12294595643",
+      from: "+17753207061",
       to: toPhoneNumber,
     });
     return message.sid;
