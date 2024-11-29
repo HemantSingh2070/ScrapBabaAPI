a2708f4601607683a7f1ba7041585adef3db4ccc
HemantSingh2070 <hemantsingh2070@gmail.com>
2024-11-29 22:39:04 +0530

    changed get to post

--- routes/scraps.js
+++ routes/scraps.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { AddScrap, GetScraps, RemoveScrap } = require("../controller/scrap");
 router.post("/", AddScrap);
-router.get("/", GetScraps);
+router.post("/get", GetScraps);
 router.delete("/", RemoveScrap);
 
 module.exports = router;
