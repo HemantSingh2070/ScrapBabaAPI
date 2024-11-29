323afd0a6bde07cbf18da46b7d7d20f25ca65ed6
HemantSingh2070 <hemantsingh2070@gmail.com>
2024-11-29 18:58:41 +0530

    added phone

--- services/message.js
+++ services/message.js
@@ -1,19 +1,19 @@
 const twilio = require("twilio");
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = "7fadd660fe778ad2a0aa04176c501d39";
+const accountSid = process.env.TWILIO_ACCOUNT_SID; // Ensure this is set in your environment variables
+const authToken = "7fadd660fe778ad2a0aa04176c501d39"; // Secure this value using environment variables
 const client = twilio(accountSid, authToken);
 
 const sendMessage = async (messageBody, toPhoneNumber) => {
   try {
     const message = await client.messages.create({
-      from: "whatsapp:+14155238886",
+      from: "+12294595643", // Replace with your Twilio phone number
       body: messageBody,
-      to: `whatsapp:${toPhoneNumber}`,
+      to: toPhoneNumber, // The recipient's phone number
     });
     return message.sid;
   } catch (error) {
-    console.error("Error sending message:", error);
+    console.error("Error sending SMS:", error);
     throw error;
   }
 };
